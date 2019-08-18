c4e732a374d1268a5919a11c769bcb935584910e
Till Schneidereit <till@tillschneidereit.net>
2019-08-18 12:45:14 +0200

    Simplify Markdown demo Rust code

--- markdown/src/lib.rs
+++ markdown/src/lib.rs
@@ -1,13 +1,9 @@
-// Example function here copied from https://docs.rs/pulldown-cmark
-
-use pulldown_cmark::{html, Options, Parser};
+use pulldown_cmark::{html, Parser};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn render(input: &str) -> String {
-    let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    let parser = Parser::new_ext(input, options);
+    let parser = Parser::new(input);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
     return html_output;
