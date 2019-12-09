fed3e8e6c546f9ba6fdd3b629ec8e4cd9797adfb
Nick Fitzgerald <fitzgen@gmail.com>
2019-12-09 15:37:38 -0800

    Fix rust demo compilation

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,7 +3,7 @@ trait WasmMarkdown {
     fn render(&mut self, input: &str) -> String;
 }
 
-fn main() -> Result<(), failure::Error> {
+fn main() -> Result<(), anyhow::Error> {
     let mut markdown = WasmMarkdown::load_file("markdown.wasm")?;
     println!("{}", markdown.render("# Hello, Rust!"));
     Ok(())
